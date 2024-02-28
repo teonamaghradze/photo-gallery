bf172767af4f3528ba0dfa1ae2fffc81b1c108e9
Teona Maghradze <maghradzeteo@gmail.com>
2024-02-28 22:01:33 +0400

    add popular images on main page

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,15 +1,55 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ImageCard from "./ImageCard";
 
 const API_URL = "https://api.unsplash.com/search/photos";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function MainPage() {
+  const [popularPhotos, setPopularPhotos] = useState([]);
+
   useEffect(() => {
-    // You can use the API key here
-    console.log(API_KEY);
+    const fetchPopularPhotos = async () => {
+      try {
+        const response = await axios.get("https://api.unsplash.com/photos", {
+          params: {
+            per_page: 20,
+            order_by: "popular",
+          },
+          headers: {
+            Authorization: `Client-ID ${API_KEY}`,
+          },
+        });
+        setPopularPhotos(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching popular photos:", error);
+      }
+    };
+
+    fetchPopularPhotos();
   }, []);
 
-  return <div>Main Page</div>;
+  return (
+    <main>
+      <h1>Searchbar</h1>
+      <section>
+        <input
+          // onChange={handleSearch}
+          type="search"
+          placeholder="search for images"
+        />
+      </section>
+
+      <section>
+        <div className="photo-grid">
+          {popularPhotos.map((photo: any) => (
+            <ImageCard photo={photo} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default MainPage;
